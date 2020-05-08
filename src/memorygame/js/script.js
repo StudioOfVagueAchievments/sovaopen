fc08785a3cb8b610d3c60e22436b1803a3206b4f
Vladislav <nepogodin.vlad@gmail.com>
2020-05-08 23:01:33 +0400

    modified:   src/memorygame/css/style.css 	modified:   src/memorygame/index.html 	modified:   src/memorygame/js/script.js

--- src/memorygame/js/script.js
+++ src/memorygame/js/script.js
@@ -1,104 +1,141 @@
-//random number generator
-
-function getRandomInt(max) 
-{
-  return Math.floor(Math.random() * Math.floor(max));
+/* JSON Result */
+var data = {
+    difficult: 0,
+    errors: 0,
+    counter: 0
 }
 
-function gameOver() 
-{
-	alert('gameOver');
-	rows = 0;
-	gameOverText = $('<div>GAME OVER</div>');
-	board.append(gameOverText);
+/* Event Click To .menu-item */
+var i = 0;
+while (i < document.getElementsByClassName('menu-item').length) {
+    document.getElementsByClassName('menu-item')[i].addEventListener('click', function() {
+        document.getElementById("menu").style.display = "none";
+
+        data.difficult = parseInt(this.dataset.lvl);
+        const attempts = parseInt(this.dataset.attempts);
+
+        buildDeck(data.difficult);
+
+        /* Change To String */
+        switch (data.difficult) {
+            case 4:
+                data.difficult = "Easy";
+                break;
+            case 6:
+                data.difficult = "Medium";
+                break;
+            default:
+                data.difficult = "Hard";
+                break;
+        } // data.difficult
+
+        /* Event Click To .cell */
+        let j = 0;
+        while (j < document.getElementsByClassName('cell').length) {
+            document.getElementsByClassName('cell')[j].addEventListener('click', function() {
+                this.classList.add("clicked");
+                this.innerHTML = this.dataset.num;
+
+                /* Attempts clicked */
+                switch (document.getElementsByClassName("clicked").length === 2) {
+                    case true:
+                        let first = document.getElementsByClassName("clicked")[0];
+                        let second = document.getElementsByClassName("clicked")[1];
+                        switch (document.getElementsByClassName("clicked")[0].dataset.num === document.getElementsByClassName("clicked")[1].dataset.num) {
+                            case true: // Good 
+                                data.counter += 1;
+
+                                first.remove();
+                                second.remove();
+                                break;
+                            default: // Bad 
+                                data.errors += 1;
+
+                                setTimeout(() => {
+                                    first.innerHTML = " ";
+                                    second.innerHTML = " ";
+
+                                    first.classList.remove("clicked");
+                                    second.classList.remove("clicked");
+                                }, 250);
+                                break;
+                        } // document.getElementsByClassName("clicked")[0].dataset.num === document.getElementsByClassName("clicked")[1].dataset.num
+                        switch (document.getElementsByClassName("cell").length === 0) {
+                            case true: // Good
+                                gameOver();
+                                break;
+                            default: // Wrong
+                                switch (data.errors == attempts) {
+                                    case true:
+                                        gameOver();
+                                        break;
+                                    default:
+                                        break;
+                                } // data.errors == attempts
+                                break;
+                        } // END ?
+                        break;
+                    default:
+                        break;
+                } // document.getElementsByClassName("clicked").length === 2
+            }, false);
+            j++;
+        } // document.getElementsByClassName('cell').length
+    }, false);
+    i++;
+} // document.getElementsByClassName('menu-item').length
+
+function buildDeck(rows) {
+    // Temporary variables
+    let pics = [];
+    let i = rows * rows / 2;
+
+    while (i--) {
+        pics[i] = i + 1;
+        pics[i + (rows * rows / 2)] = i + 1;
+    }
+
+    /* Randomizing */
+    i = rows * rows;
+    while (i--) {
+        let rand = getRandomInt(rows * rows / 2);
+        let temp = pics[i];
+        pics[i] = pics[rand];
+        pics[rand] = temp;
+    }
+
+    /* Styling */
+    switch (data.difficult) {
+        case 4:
+            document.getElementById("body").innerHTML = "<div id=deck style=grid-template-columns:repeat(4,1fr);></div>";
+            document.getElementById("body").innerHTML += "<span id=attCounter>0</span>";
+            break;
+        case 6:
+            document.getElementById("body").innerHTML = "<div id=deck style=width:grid-template-columns:repeat(6,1fr);></div>";
+            document.getElementById("body").innerHTML += "<span id=attCounter>0</span>";
+            break;
+        case 8:
+            document.getElementById("body").innerHTML = "<div id=deck style=width:grid-template-columns:repeat(8,1fr);></div>";
+            document.getElementById("body").innerHTML += "<span id=attCounter>0</span>";
+            break;
+    }
+
+    /* Draw cell */
+    i = rows * rows;
+    while (i--) {
+        let space = " ";
+        document.getElementById("deck").innerHTML += "<span" + space + "class=cell" + space + "data-num=" + pics[i] + "></span>";
+    }
 }
 
-var openedCounter = 0;
-var attemptsMax = 0;
-var attempts = 0;
-
-function onMenuItemClick() {
-	$("#menu").hide();
-	var menuItem = $(this);
-	var rows = menuItem.data('lvl');
-	attempts = menuItem.data('attempts');
-	console.log(rows, attemptsMax);
-	buildBoard(rows);
-};
-
-function getSum(a, b){
-	return a+b;
-}
-var counter = 0;
-//on cell click function
-function onCellClick(rows) {
-
-	if($('.clicked').length >= 2) return;
-	
-	var cell = $(this);
-	cell.addClass('clicked');
-	cell.text(cell.data('num'));
-	var clickedCells = $('.clicked');
-	//attempts modes
-
-	if(clickedCells.length == 2 ) 
-	{
-		var c1 = $(clickedCells[0]);
-		var c2 = $(clickedCells[1]);
-		if(c1.data('num')== c2.data('num')) {
-			c1.addClass('solved');
-			c2.addClass('solved');
-		}
-		setTimeout(function() {
-			c1.text(' ').removeClass('clicked');
-			c2.text(' ').removeClass('clicked');
-
-			attempts--;
-		},300);	
-		if (attempts <= 0) gameOver();
-	}
-}
-function buildBoard(rows) {
-	var pics = [];
-
-	for (var i = 0; i< rows*rows/2; i++)
-	{
-		pics[i] = i + 1;
-		pics[i+(rows*rows/2)] = i + 1;
-	}
-	var temp;
-	var rand;//random number for shuffling
-	for (var i = 0; i < rows*rows; i++)//randomizing 
-	{
-		rand = getRandomInt(rows*rows/2);
-		temp = pics[i];
-		pics[i] = pics[rand];
-		pics[rand] = temp;
-	}
-	var cellSize = 100+20+10;
-
-	var board = $('#board');
-	//debugger;
-	for(var i = 0; i < rows*rows; i++) {
-		var cell = $('<div data-num='+ pics[i] +' class="cell"> </div>');//cell initialization
-		//var num = getRandomInt(rows*rows/2);
-		//console.log(num);
-		//cell.data('img number', pics[i]);
-		board.append(cell);
-	}
-	//style for cells
-	board.css("width", cellSize*rows)
-	//cell.css(text)
-	// var settings1 = $('.rowsChanging') 
-	var cells = $('.cell');
-	cells.click(onCellClick);
-	
-}
-function startGame() {
-	var menuItems = $('.menu-item');
-
-	console.log('here', menuItems);
-	menuItems.click(onMenuItemClick);
+function gameOver() {
+    document.getElementById("body").innerHTML = "<div id=result style=grid-template-columns:repeat(3,1fr);>" +
+        "difficult:" + data.difficult + " " +
+        "errors:" + data.errors + " " +
+        "success:" + data.counter + 
+        "<a href=https://studioofvagueachievments.github.io/sovaopen/ role=button>Go Home</a>" + "</div>";
 }
 
-$(document).ready(startGame);
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
